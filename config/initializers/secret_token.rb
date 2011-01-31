699cd16f98bca860a1a79d238ff4de7d5641fbd9
U-ASH-PC\ASH <ASH@ASH-PC.(none)>
2011-01-31 06:43:26 +0000

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '997646d2398e7cf62d3e863a71da3779979f5917fe7e192abd6eb18a0018a98acfc95f0fb59385076ac644da1e83f9e357a7be8759af13b43d5d6bcea5fa29b2'

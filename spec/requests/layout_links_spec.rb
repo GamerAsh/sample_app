70ca684a1a6d648b04910c1c8f4a5ccb6c75b9ea
Ashley Birtwell <ash@ubuntu.(none)>
2011-02-23 14:32:13 -0800

    Added Routes to sign-in, and sessions controller

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,6 +27,10 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
 end
+    it "should have a signin page at '/signup'" do
+    get '/signin'
+    response.should have_selector('title', :content => "Sign in")
+end
 
     it "should have the right links on layout'" do
     visit root_path
